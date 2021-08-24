9f7910613884baa1ba9099efc169e86e3b834844
KKIMDoHyun <kdh5998@naver.com>
2021-08-25 02:09:49 +0900

    문제 68번과 70번

--- 61~70/quiz68.js
+++ 61~70/quiz68.js
@@ -1,2 +1,25 @@
 // 버스 도착시간이 몇 분 남았는지 알려주는 프로그램을 만들어라.
 // 버스 시간표와 현재 시간이 주어졌을 때 버스 도착 시간이 얼마나 남았는지 알려주는 프로그램을 만들어라.
+
+const 버스시간 = ["12:30", "13:20", "14:13"];
+const 기준시간 = "12:40";
+
+function solution(버스시간, 기준시간) {
+    let answer = [];
+    기준시간 = 기준시간.split(":").map(n => parseInt(n,10));
+    기준시간 = 기준시간[0] * 60 + 기준시간[1];
+    for(let i in 버스시간) {
+        let time = 버스시간[i].split(":").map(n => parseInt(n,10));
+        time = time[0] * 60 + time[1];
+        if (time < 기준시간) {
+            answer.push('지나갔습니다.');
+        } else {
+            let 시간 = parseInt((time - 기준시간) / 60, 10);
+            let 분 = parseInt((time - 기준시간) % 60, 10)
+            answer.push(String(시간).padStart(2, 0) + '분');
+        }
+    }
+    return answer;
+}
+
+console.log(solution(버스시간, 기준시간))
--- 61~70/quiz70.js
+++ 61~70/quiz70.js
@@ -1,2 +1,36 @@
 // 행렬 2개가 주어졌을 때 곱할 수 있는 행렬인지 확인하고 곱할 수 있다면 결과를 출력하고 없다면 -1을 출력하라.
 
+let a = [
+    [1, 2],
+    [3, 4]
+]
+
+let b = [
+    [5, 6],
+    [7, 8]
+]
+
+function solution(a, b) {
+    let result = [];
+    const len = a.length;
+
+    if(a[0].length === b.length) {
+        let result = [];
+        for(let i = 0; i < len; i++) {
+            let row = [];
+            for(let j = 0; j < len; j++) {
+                let el = 0;
+                for(let k = 0; i < len; k++) {
+                    el += a[i][k] * b[k][j];
+                }
+                row.push(el);
+            }
+            result.push(row);
+        }
+        return result;
+    } else {
+        return -1;
+    }
+}
+
+console.log(solution(a, b));
